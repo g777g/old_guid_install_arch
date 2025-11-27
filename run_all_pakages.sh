65dc8e8188b439682e17203b501d23395a975a40
g777g <76852433+g777g@users.noreply.github.com>
2025-11-27 11:29:53 +0200

    Update run_all_pakages.sh

--- run_all_pakages.sh
+++ run_all_pakages.sh
@@ -1,2 +1,2 @@
-#! /bin/sh
-sudo pacmna -S ly xorg xorg-xinit micro mousepad bspwm sxhkd thunar alacritty polybar brightnessctl nm-connection-editor networkmanager pulseaudio  pipewire pipewire-pulse bluez bluez-utils blueman playerctl ttf-jetbrains-mono-nerd scrot light zip unzip feh picom udisks2 gvfs gvfs-mtp gvfs-gphoto2 gvfs-afc polkit polkit-gno 
+sudo pacmna -S ly xorg xorg-xinit micro mousepad bspwm sxhkd thunar alacritty polybar brightnessctl nm-connection-editor networkmanager pulseaudio  pipewire pipewire-pulse bluez bluez-utils blueman playerctl ttf-jetbrains-mono-nerd scrot zip unzip feh picom udisks2 gvfs gvfs-mtp gvfs-gphoto2 gvfs-afc polkit
+
